d9053f32b705bb1eee93aed0abefbaa91aedb740
John Lewis <shiv3rcubesolver@gmail.com>
2021-04-21 13:09:08 -0600

    got massive text document of most used words, added description of main code

---
+++ passwordgen.py
@@ -0,0 +1,6 @@
+import random
+
+#a "simple" program to generate random passwords a la that one xkcd.
+#takes a text file of the 10 thousand most used words and grabs 4 random ones.
+#it might be too much for python, might need to use a compiled language.
+
